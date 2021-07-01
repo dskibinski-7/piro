420e92558a8286829f0a2fb0e40003a1952afc4a
Dawid <dawid.r.skibinski@student.put.poznan.pl>
2021-07-01 22:39:55 +0200

    Base found

--- proj1.py
+++ proj1.py
@@ -3,6 +3,7 @@ from skimage import io
 from skimage import measure 
 from matplotlib import pyplot as plt
 import cv2
+import numpy as np
 
 
 def read_images(num_of_images, path):
@@ -34,7 +35,7 @@ def find_vertices(img):
         approx = cv2.approxPolyDP(cnt, 0.005 * cv2.arcLength(cnt, True), True)
       
         # draws boundary of contours.
-        cv2.drawContours(img, [approx], 0, (0, 0, 255), 5) 
+        #cv2.drawContours(img, [approx], 0, (0, 0, 255), 5) 
       
         # Used to flatted the array containing
         # the co-ordinates of the vertices.
@@ -47,7 +48,7 @@ def find_vertices(img):
                 y = n[i + 1]
       
                 vertices.append((x,y))
-                img = cv2.circle(img, (x,y), radius=3, color=(0, 255, 0), thickness=-1)
+                #img = cv2.circle(img, (x,y), radius=3, color=(0, 255, 0), thickness=-1)
                 
     
             i = i + 1
@@ -56,12 +57,40 @@ def find_vertices(img):
 
 
 
+def find_base(vertices):
+    
+    #base is probably? the longest segment
+    max_dist = 0
+    
+    for v in range(len(vertices)):
+        x_a = vertices[v][0]
+        y_a = vertices[v][1]
+        
+        try:
+            x_b = vertices[v+1][0]
+            y_b = vertices[v+1][1]
+        except IndexError:
+            x_b = vertices[0][0]
+            y_b = vertices[0][1]
+            
+        current_dist = np.sqrt((x_b-x_a)**2+(y_b-y_a)**2)
+        
+        if current_dist > max_dist:
+            max_dist = current_dist
+            base = [(x_a,y_a),(x_b,y_b)]
+            
+            
+    return max_dist, base
+
+
+def find_sides(vertices, base):
+    pass
 
 
 features = {}
 
 
-images, smallest_shape = read_images(6, r"C:\Users\skibi\OneDrive\Dokumenty\Informatyka\Piro\Proj1\set0") 
+images, smallest_shape = read_images(6, ".\set0") 
 
 i=0
 for image in images:
@@ -70,14 +99,20 @@ for image in images:
 
     
     vertices = find_vertices(image)
-    features[i] = {'num_of_vertices':len(vertices)}
+    base_length, base_cord = find_base(vertices)
+    #sides = find_sides...
+    features[i] = {'num_of_vertices':len(vertices), 'base_length':base_length}
+    
+
+
     
     i+=1
 
 
 #focus na liczbie krawedzi i wierzcholkow
 
-            
+#plt.imshow(image)
+#plt.show           
         
         
 #DODATKOWO - KRAWEDZIE (PODSTAWA I BOKI) POWINNY BYC DLUGOSCIAMI PODOBNYMI DO ODPOWIADAJACEGO PROSTOKAT
