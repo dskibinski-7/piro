cc516042f988050f9ca3280a0076448eedc531b2
Dawid <dawid.r.skibinski@student.put.poznan.pl>
2021-07-06 17:13:13 +0200

    args from cmd; a little bit of cleaning

--- proj1.py
+++ proj1.py
@@ -1,8 +1,9 @@
 import os
-from matplotlib import pyplot as plt
+#from matplotlib import pyplot as plt
 import cv2
 import numpy as np
 import statistics
+import sys
 
 def read_images(num_of_images, path):
     images = []
@@ -52,62 +53,7 @@ def find_vertices(img):
 #    plt.show
     ##</POMOCNICZE>
     return vertices
-
-def find_vertices_on_curve(img, first_side_vertex, second_side_vertex, smallest_width):
-    
-    #ze skalowaniem
-    #img = scale_image(img, smallest_width)
-    
-    imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(imgray, 127, 255, 0)
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)    
-    
-    vertices = []
-    
-    # Going through every contours found in the image.
-    for cnt in contours :
-
-        approx = cv2.approxPolyDP(cnt, 0.004 * cv2.arcLength(cnt, True), True)
-        
-        #pomocnicze 
-        #i=0
-        for appr in approx:
-            x=appr[0][0]
-            y=appr[0][1]                            
-            vertices.append((x,y))
-            #img = cv2.circle(img, (x,y), radius=1, color=(255,0,255), thickness=-1)
-            #pomocnicze
-            #cv2.putText(img, str(i), (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.3, (255, 0, 0))
-            #pomocnicze
-            #i+=1
-
-        break
-    
-    #dodatkowe rysowanko dla obczajki:
-#    plt.figure()
-#    plt.imshow(img)
-#    plt.show
-    
-    fs_vertex_ix = vertices.index(first_side_vertex)
-    ss_vertex_ix = vertices.index(second_side_vertex)
-    
-    if fs_vertex_ix > ss_vertex_ix:
-        vertices = vertices[ss_vertex_ix:fs_vertex_ix+1]
-    else:
-        vertices = vertices[:fs_vertex_ix+1] + vertices[ss_vertex_ix:]
-        
-    #pomocnicze wyswietlenie:
-#    i=0
-#    for vertex in vertices:
-#        img = cv2.circle(img, vertex, radius=1, color=(255,0,255), thickness=-1)
-#        cv2.putText(img, str(i), vertex, cv2.FONT_HERSHEY_COMPLEX, 0.3, (255, 0, 0))
-#        i+=1
-#    plt.figure()
-#    plt.imshow(img)
-#    plt.show
-    #tylko wierzchołki z krzywej - sprawdzic dlugosci ile sie pokrywa - odpowiednie boki == wierzcholki od podstawy
-    return vertices
-    
+  
 
 def find_base(vertices):
     
@@ -131,10 +77,8 @@ def find_base(vertices):
             max_dist = current_dist
             base = [(x_a,y_a),(x_b,y_b)]
             
-    #try:
     return max_dist, base
-    #except UnboundLocalError:
-    #    pass
+
 
 def find_sides(vertices, base):
     
@@ -298,80 +242,80 @@ def compare_vertex_base_distances(features):
                 sum = i_base_vertex_distances[v_i] + scaled_j_base_vertex_distances[v_j]
                 totals.append(sum)
                 
-            #print('Totals for ' + str(i) + ' and ' + str(j) + ': ' + str(totals))
-            #print('*'*12)
+#            print('Totals for ' + str(i) + ' and ' + str(j) + ': ' + str(totals))
+#            print('*'*12)
             comparison[i][j] = statistics.variance(totals)
             
-            #totals = []
     for c in range(len(comparison)):
         features[c]["match_by_length_vertex_base_variance"] = sorted(comparison[c], key=comparison[c].get)
-            #porownywac wierzcholki ktorych odleglosc od sciany bocznej jest ~~taka sama (najblizsza do oczekiwanej)
+
             
-            
-#            for i_distance in i_distances:
-#                for j_distance in scaled_j_distances:
-#                    sum = round(i_distance+j_distance,2)
-#                    totals.append(sum)
+
+#def check_similarity_scale(features):
+#    #bierzemy dlugosc podstawy danego image i porownujemy do dlugosci pozostalych
+#    #powstaje nam k
+#    
+#    similarity = {}
+#    
+#    #skalujemy przez k pozostale image i sprawdzamy gdzie dlugosci bokow pasuja do siebie
+#    for i in range(len(features)):
+#        i_base = features[i]['base_length']
+#        i_first_side = features[i]['first_side_length']
+#        i_second_side = features[i]['second_side_length']
+#        
+#        #shorter longer?
+#        
+#        similarity[i] = {}
+#        for j in range(len(features)):           
+#          
+#            if i == j:
+#                continue
+#            j_base = features[j]['base_length']
+#            j_first_side = features[j]['first_side_length']
+#            j_second_side = features[j]['second_side_length']
+#            #skala
+#            k = i_base/j_base
 #
-#            print(totals)
-
-def check_similarity_scale(features):
-    #bierzemy dlugosc podstawy danego image i porownujemy do dlugosci pozostalych
-    #powstaje nam k
-    
-    similarity = {}
-    
-    #skalujemy przez k pozostale image i sprawdzamy gdzie dlugosci bokow pasuja do siebie
-    for i in range(len(features)):
-        i_base = features[i]['base_length']
-        i_first_side = features[i]['first_side_length']
-        i_second_side = features[i]['second_side_length']
-        
-        #shorter longer?
-        
-        similarity[i] = {}
-        for j in range(len(features)):           
-          
-            if i == j:
-                continue
-            j_base = features[j]['base_length']
-            j_first_side = features[j]['first_side_length']
-            j_second_side = features[j]['second_side_length']
-            #skala
-            k = i_base/j_base
-
-            
-            scaled_j_first_side = k*j_first_side
-            scaled_j_second_side = k*j_second_side
-            
-            
-            sum_l_side = i_first_side + scaled_j_second_side
-            sum_r_side = i_second_side + scaled_j_first_side
-            
-            #CZY MOŻE PODZIELIC TO JESZCZE PRZEZ 'COS'? ze wzgledu na rozne wielkosci tych prostokatow roznica dlugosci moze nie byc tak miarodajna
-            abs_diff = abs(sum_l_side - sum_r_side)
-            
-            similarity[i][j] = abs_diff
-
-            
-            
-    for s in range(len(similarity)):
-        features[s]["match_by_sides_similarity"] = sorted(similarity[s], key=similarity[s].get)
+#            
+#            scaled_j_first_side = k*j_first_side
+#            scaled_j_second_side = k*j_second_side
+#            
+#            
+#            sum_l_side = i_first_side + scaled_j_second_side
+#            sum_r_side = i_second_side + scaled_j_first_side
+#            
+#            #CZY MOŻE PODZIELIC TO JESZCZE PRZEZ 'COS'? ze wzgledu na rozne wielkosci tych prostokatow roznica dlugosci moze nie byc tak miarodajna
+#            abs_diff = abs(sum_l_side - sum_r_side)
+#            
+#            similarity[i][j] = abs_diff
+#
+#            
+#            
+#    for s in range(len(similarity)):
+#        features[s]["match_by_sides_similarity"] = sorted(similarity[s], key=similarity[s].get)
             
     
+
+
+
+
+
+#path_to_images = '.\set0'
+#amount_of_images = 6
+
+
+try:
+    path_to_images = str(sys.argv[1])
+    amount_of_images = int(sys.argv[2])
+    images, smallest_width = read_images(amount_of_images, path_to_images) 
+except:
+    print('Invalid input')
+    exit()
     
-    #funckja niech dorzuca do slownika features do ktorego dany obrazek najbardziej by pasowal na podstawie skali podobienstwa
-
-
-
-features = {}
-#SPRAWDZIC JESZCZE PROBLEM 9 ELEMENTU W SET6!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-images, smallest_width = read_images(20, ".\set7") 
-
+features = {}  
 i=0
 for image in images:
 
-    #image = scale_image(image,smallest_width)
     
     vertices = find_vertices(image)
     base_length, base_cord = find_base(vertices)
@@ -386,38 +330,18 @@ for image in images:
             'second_side_length':second_side_length, 'base_vertices_distances': base_vertices_distances,
             'first_side_vertices_distances':first_side_vertices_distances, 'second_side_vertices_distances':second_side_vertices_distances}
     
-
-
-
-    #połączyć liczbe wierzcholkow z smiliratiy zeby zwiekszyc prawdopodobienstwo trafienia
-    #(w wierzcholkach najpierw te co idealnie featuja a potem te co maja coraz wieksza roznice wierzcholkow)
-    #
-    
-    
-    
-    #moremoremorevertices = find_vertices_on_curve(image, first_side_vertex, second_side_vertex, smallest_width)
-
-    #features[i]['vertices_v2'] = len(moremoremorevertices)
-    #wyswietlenei obrazow
-#    image = cv2.circle(image, base_cord[0], radius=1, color=(0, 255, 0), thickness=-1)
-#    image = cv2.circle(image, base_cord[1], radius=1, color=(170, 245, 145), thickness=-1)
-#    
-#    image = cv2.circle(image, first_side_vertex, radius=1, color=(255, 0, 0), thickness=-1)
-#    image = cv2.circle(image, second_side_vertex, radius=1, color=(245, 145, 180), thickness=-1)
-#
-#    plt.figure()
-#    plt.imshow(image)
-#    plt.show 
-
-    
+  
     i+=1
 
-similarity = check_similarity_scale(features)
+#similarity = check_similarity_scale(features)
 compare_vertex_base_distances(features)
 
 #wyprintuj wszystkie
 for i in range(len(features)):
-    print(features[i]['match_by_length_vertex_base_variance'])
+    matches = features[i]['match_by_length_vertex_base_variance']
+    for match in matches:
+        print(match, end=' ', sep=' ')
+    print('')
 
 #wyprintuj tylko peirwsze
 #a=[]
